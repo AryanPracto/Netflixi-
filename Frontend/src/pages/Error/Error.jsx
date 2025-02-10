d45da678ea2bf6b2c3e61a939c9646fa2a2d9f2c
Aryan <aryan@Apples-MacBook-Pro.local>
2025-02-10 10:37:15 +0530

    fourth commit

--- Frontend/src/pages/Error/Error.jsx
+++ Frontend/src/pages/Error/Error.jsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
 import "./Error.css"; // Import CSS file
 
+const onClickHandler=()=>{
+  if(localStorage.getItem('token')){
+    localStorage.removeItem('token');
+  }
+  if(localStorage.getItem('userName')){
+    localStorage.removeItem('userName');
+  }
+}
+
 const Error = () => {
   return (
     <div className="error-container">
       <h1 className="error-title">404 - Page Not Found</h1>
       <p className="error-message">Oops! The page you’re looking for doesn’t exist.</p>
-      <Link to="/" className="error-button">Go Home</Link>
+      <Link to="/" className="error-button" onClick={onClickHandler}>Go Home</Link>
     </div>
   );
 };
