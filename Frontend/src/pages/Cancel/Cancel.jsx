2b15e82031f18ab5affa97e8c708586ff7ec21c0
Aryan <aryan@Apples-MacBook-Pro.local>
2025-02-10 15:19:13 +0530

    seventh commit

--- Frontend/src/pages/Cancel/Cancel.jsx
+++ Frontend/src/pages/Cancel/Cancel.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Cancel.css'
 
 const Cancel = () => {
   return (
-    <div>
-      your payment failed man 
+    <div className='mn'>
+      <h1>your payment failed man</h1> 
     </div>
   )
 }
